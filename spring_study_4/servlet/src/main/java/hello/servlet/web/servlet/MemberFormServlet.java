9981fbe67d4edd08a11ff129ca6b1f2fcc7189a9
BH946 <kobh2895@gmail.com>
2023-08-20 19:49:20 +0900

    add : 서블릿으로 회원(form)데이터 동작 확인, 회원저장, 회원조회 개발

---
+++ spring_study_4/servlet/src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,45 @@
+package hello.servlet.web.servlet;
+
+
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * HttpServlet 상속 필수 -> Ctrl+O (service 오버라이드)
+ * form 데이터 넘어가는지 확인용
+ */
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html"); // html로 반환목적
+        response.setCharacterEncoding("utf-8"); // 필수
+
+        PrintWriter w = response.getWriter();
+        w.write("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                "    username: <input type=\"text\" name=\"username\" />\n" +
+                "    age:      <input type=\"text\" name=\"age\" />\n" +
+                "    <button type=\"submit\">전송</button>\n" +
+                "</form>\n" +
+                "</body>\n" +
+                "</html>\n");
+
+    }
+}
