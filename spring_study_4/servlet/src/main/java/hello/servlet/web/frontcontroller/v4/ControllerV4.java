407849eeb42cd14e4b3c027b35b16f1af3732c60
BH946 <kobh2895@gmail.com>
2023-08-20 21:05:54 +0900

    add : (MVC프레임워크 만들기) 실용적인 컨트롤러(반환을 ModelView -> String으로 변환) - v4

---
+++ spring_study_4/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,13 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import java.util.Map;
+
+
+/**
+ * MyView 나 ModelView 도 아닌 이번엔 String으로 반환값을 바꿨음.
+ * => 개발자 편의를 위해서 수정한 것 (매번 ModelView를 반환하는것은 편리하지는 않기때문)
+ */
+public interface ControllerV4 {
+
+    String process(Map<String, String> paramMap, Map<String, Object> model);
+}
