e9bbb8644f1357eea5c604f7f569df044b7bb81f
BH946 <kobh2895@gmail.com>
2023-08-21 18:49:29 +0900

    add : HTTP 요청 파라미터 1 - @RequestParam 자세히 및 파라미터 필수, 기본값, Map 형태

---
+++ spring_study_4/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,107 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    /**
+     * hello-from.html 에서 넘어온 form 데이터 -> 이 역시 파라미터 형식으로 넘어옴 (body에 담기지만!)
+     * 또한, 그냥 URL로 파라미터 형식으로 넘어온 데이터
+     * 결론은 둘다 getParameter() 가 사용 가능
+     */
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        response.getWriter().write("ok");
+    }
+
+    /**
+     * getParameter 가 아닌 @RequestParam
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge) {
+
+        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam("age") -> @RequestParam 로 생략
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username,
+            @RequestParam int age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * @RequestParam 가 생략 -> 단, 오히려 햇갈릴 수도 있어서 본인은 넣는걸 권장
+     * 단, 완전 생략때는 required = false 가 기본값이 된다.
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * 생략아닐때는 required 확인해보면 true 가 기본임을 알 수 있다.
+     * 여기까지 전부 같은 로그를 띄워야 정상!
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * 기본값이 가능!! -> 이것의 장점은 /request-param-default?username= 처럼
+     * username이 "" 빈값으로 넘어오는 경우도 인식해서 기본값으로 적용해줌!!
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true, defaultValue = "guest") String username,
+            @RequestParam(required = false, defaultValue = "-1") int age) {
+
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    /**
+     * 파라미터를 Map 형태로도 받아올 수 있다!
+     * 단, 데이터가 확실히 1개가 아니면 MultiValueMap 을 사용해주자
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+}
